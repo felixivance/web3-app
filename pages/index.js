bc9038f1f323586a9ee2d1302c01f86c771b3a76
Felix <felixrunye@gmail.com>
2021-12-14 22:17:29 +0300

    updated navbar import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       </Head>
 
       {/* navbar */}
+      <Navbar />
       {/* main */}
       {/* footer */}
     </div>
