fb7528e3fa33cd70523612b7e166ccaf8953207d
Felix <felixrunye@gmail.com>
2021-12-14 22:22:41 +0300

    navbar update

---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+import { UserIcon } from '@heroicons/react/outline'
+
+function Navbar() {
+    return (
+        <div className="">
+            <div className="">
+
+                {/* avatar */}
+                <UserIcon className='h-5'/>
+                {/* logo */}
+                <h2>CryptoVerse</h2>
+                {/* <button className="">
+
+                </button> */}
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Crypto App</title>
         <link rel="icon" href="/favicon.ico" />
