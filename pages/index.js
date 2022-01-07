8822b180eef26817fd4a939535f00171c97abf46
Felix <felixrunye@gmail.com>
2022-01-07 16:59:42 +0300

    index update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import CollectionCard from '../components/CollectionCard'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
 
       {/* navbar */}
       <Navbar />
+      <CollectionCard id="0" name="felx" traits={[{ 'value': '0' }]} image='' />
       {/* main */}
       {/* footer */}
     </div>
