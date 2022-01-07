e0a58bca23b312063c413aba15f311c72a942b8b
Felix <felixrunye@gmail.com>
2022-01-07 16:49:09 +0300

    addded images and collection card

--- components/CollectionCard.js
+++ components/CollectionCard.js
@@ -1,7 +1,24 @@
-function CollectionCard() {
+import { weth } from '../public/images/weth.png';
+
+function CollectionCard({ id, name, traits, image }) {
     return (
         <div>
+            <img src={image} alt="" />
+            {/* details */}
+            <div>
 
+            </div>
+            {/* name */}
+            <div>
+                {name}
+                <div>
+                    .#{id}
+                </div>
+            </div>
+            {/* price container */}
+            <div>
+                <img src={weth} alt="" />
+            </div>
         </div>
     )
 }
