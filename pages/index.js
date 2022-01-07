4618e4dbf55f3616276c5585916c491fbdc7e656
Felix <felixrunye@gmail.com>
2022-01-07 20:54:21 +0300

    updated crypto view

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="bg-black h-screen ">
       <Head>
-        <title>Crypto App</title>
+        <title>NFT App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
