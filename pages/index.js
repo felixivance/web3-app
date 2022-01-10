576148c3668170023385a1aaf1ce6381c0f85e89
Felix <felixrunye@gmail.com>
2022-01-10 20:37:44 +0300

    updated fetching data

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import CollectionCard from '../components/CollectionCard'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react';
-import { axios } from 'axios';
+import axios from 'axios';
 
 export default function Home() {
 
@@ -10,13 +10,14 @@ export default function Home() {
 
 
   useEffect(() => {
-    const getMyNfts = async => {
-
+    const getMyNfts = async () => {
+      const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x02C05dDc6480b24a27085D7D335a43F466b6436d&order_direction=asc')
+      console.log(openSeaData.data.assets)
     }
 
-    return () => {
-      cleanup
-    }
+
+
+    return getMyNfts();
   }, [])
 
   return (
